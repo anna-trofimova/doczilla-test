785147c37f6eacc856db72cf2a7d5ff2ac2df5d5
anna-trofimova <cobrana1000@gmail.com>
2026-02-16 15:28:03 +0100

    Added: HTTP handlers and upload method

--- task2-file-service/src/main/java/com/example/fileservice/Main.java
+++ task2-file-service/src/main/java/com/example/fileservice/Main.java
@@ -5,25 +5,15 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
-        //Create server at 8000 port
         HttpServer server = HttpServer.create(
                 new InetSocketAddress(8080),
                 0
         );
 
-        server.createContext("/", exchange -> {
-            String response = "OK";
+        server.createContext("/", new RootHandler());
+        server.createContext("/upload", new UploadHandler());
 
-            exchange.sendResponseHeaders(200, response.length());
-
-            try (OutputStream os = exchange.getResponseBody()) {
-                os.write(response.getBytes());
-            }
-        });
-
-        // Start server
         server.start();
 
         System.out.println("Server started on http://localhost:8080");
