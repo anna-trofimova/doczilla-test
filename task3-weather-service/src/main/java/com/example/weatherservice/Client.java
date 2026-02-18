072b3c9ed0d2348bc9b664745e870c8525963d65
anna-trofimova <cobrana1000@gmail.com>
2026-02-18 18:14:37 +0100

    Update: added a weather forecast request and response

--- task3-weather-service/src/main/java/com/example/weatherservice/Client.java
+++ task3-weather-service/src/main/java/com/example/weatherservice/Client.java
@@ -5,26 +5,56 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Locale;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 
 public class Client {
     private final String GEOCODING_URL = "https://geocoding-api.open-meteo.com/v1/search?name=%s";
+    private final String WEATHER_URL = "https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&hourly=temperature_2m";
     private final HttpClient client;
 
     public Client(){
         client = HttpClient.newHttpClient();
     }
 
-    public String findCity(String city) throws IOException, InterruptedException {
+    // Get city coordination
+    public JSONObject findCity(String city) throws IOException, InterruptedException {
         String url = String.format(GEOCODING_URL, city);
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .GET()
                 .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        return response.body();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JSONObject json = new JSONObject(response.body());
+
+
+        // Check for empty results
+        JSONArray results = json.optJSONArray("results");
+        if (results == null || results.length() == 0) return null;
+
+        return results.getJSONObject(0);
+    }
+
+    // Get weather information
+    public JSONObject getWeather(double latitude, double longitude) throws IOException, InterruptedException {
+        String url = String.format(Locale.US,
+                "https://api.open-meteo.com/v1/forecast?latitude=%.6f&longitude=%.6f&hourly=temperature_2m",
+                latitude, longitude);
+
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return new JSONObject(response.body());
     }
 
 
--- task3-weather-service/src/main/java/com/example/weatherservice/Main.java
+++ task3-weather-service/src/main/java/com/example/weatherservice/Main.java
@@ -1,9 +1,34 @@
 package com.example.weatherservice;
 
+import org.json.JSONObject;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Client client = new Client();
-        System.out.println(client.findCity("Berlin"));
-        System.out.println(client.findCity("Moscow"));
+
+        String cityName = "Berlin";
+        JSONObject city = client.findCity(cityName);
+
+        if (city == null) {
+            System.out.println("Город не найден: " + cityName);
+            return;
+        }
+
+        // Print city information
+        System.out.println("Город найден:");
+        System.out.println(city.toString(2));
+
+        double lat = Double.parseDouble(city.get("latitude").toString());
+        double lon = Double.parseDouble(city.get("longitude").toString());
+
+
+        System.out.println("Используем координаты: Latitude = " + lat + ", Longitude = " + lon);
+
+        // Get weather
+        JSONObject weather = client.getWeather(lat, lon);
+
+        System.out.println("Прогноз погоды (24 часа):");
+        System.out.println(weather.toString(2));
+
     }
 }
