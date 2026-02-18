62b06b458291377075112b6c60a887ffce88d918
anna-trofimova <cobrana1000@gmail.com>
2026-02-18 18:39:05 +0100

    Added: cache weather data using Redis

--- task3-weather-service/src/main/java/com/example/weatherservice/Client.java
+++ task3-weather-service/src/main/java/com/example/weatherservice/Client.java
@@ -10,15 +10,22 @@ import java.util.Locale;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import redis.clients.jedis.Jedis;
+
 
 
 public class Client {
     private final String GEOCODING_URL = "https://geocoding-api.open-meteo.com/v1/search?name=%s";
-    private final String WEATHER_URL = "https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&hourly=temperature_2m";
     private final HttpClient client;
+
+    private final Jedis jedis; // Redis client
+    private final int TTL_SECONDS = 15 * 60; // 15 mins
+
+
 
     public Client(){
         client = HttpClient.newHttpClient();
+        jedis = new Jedis("localhost", 6379);
     }
 
     // Get city coordination
@@ -56,6 +63,33 @@ public class Client {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         return new JSONObject(response.body());
     }
+
+    // Weather forecast's cache in Redis
+    public JSONObject getWeatherForCity(String cityName) throws IOException, InterruptedException {
+        String key = "weather:" + cityName.toLowerCase();
+
+        // Check cache
+        String cached = jedis.get(key);
+        if (cached != null) {
+            System.out.println("Возвращаем прогноз из Redis для " + cityName);
+            return new JSONObject(cached);
+        }
+
+        // If it is not in cache
+        JSONObject city = findCity(cityName);
+        if (city == null) return null;
+
+        double lat = city.getDouble("latitude");
+        double lon = city.getDouble("longitude");
+
+        JSONObject weather = getWeather(lat, lon);
+
+        // Save in Redis with TTL
+        jedis.setex(key, TTL_SECONDS, weather.toString());
+
+        return weather;
+    }
+
 
 
 }
--- task3-weather-service/src/main/java/com/example/weatherservice/Main.java
+++ task3-weather-service/src/main/java/com/example/weatherservice/Main.java
@@ -7,28 +7,22 @@ public class Main {
         Client client = new Client();
 
         String cityName = "Berlin";
-        JSONObject city = client.findCity(cityName);
 
-        if (city == null) {
+        JSONObject weather = client.getWeatherForCity(cityName);
+
+        if (weather == null) {
             System.out.println("Город не найден: " + cityName);
             return;
         }
 
-        // Print city information
-        System.out.println("Город найден:");
-        System.out.println(city.toString(2));
-
-        double lat = Double.parseDouble(city.get("latitude").toString());
-        double lon = Double.parseDouble(city.get("longitude").toString());
-
-
-        System.out.println("Используем координаты: Latitude = " + lat + ", Longitude = " + lon);
-
-        // Get weather
-        JSONObject weather = client.getWeather(lat, lon);
-
-        System.out.println("Прогноз погоды (24 часа):");
+        System.out.println("Прогноз погоды для города " + cityName + " (24 часа):");
         System.out.println(weather.toString(2));
 
+        JSONObject weather2 = client.getWeatherForCity(cityName);
+        System.out.println("Второй вызов (из Redis):");
+        System.out.println(weather2.toString(2));
     }
-}
+
+
+
+    }
