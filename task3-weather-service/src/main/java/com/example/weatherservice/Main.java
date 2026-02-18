5601150eaf29d80ebb183f2b9df5a128e38263c1
anna-trofimova <cobrana1000@gmail.com>
2026-02-18 19:10:06 +0100

    Added: Weather forecast chart bar

--- task3-weather-service/src/main/java/com/example/weatherservice/Main.java
+++ task3-weather-service/src/main/java/com/example/weatherservice/Main.java
@@ -5,8 +5,7 @@ import org.json.JSONObject;
 public class Main {
     public static void main(String[] args) throws Exception {
         Client client = new Client();
-
-        String cityName = "Berlin";
+        String cityName = "Moscow";
 
         JSONObject weather = client.getWeatherForCity(cityName);
 
@@ -18,11 +17,7 @@ public class Main {
         System.out.println("Прогноз погоды для города " + cityName + " (24 часа):");
         System.out.println(weather.toString(2));
 
-        JSONObject weather2 = client.getWeatherForCity(cityName);
-        System.out.println("Второй вызов (из Redis):");
-        System.out.println(weather2.toString(2));
+        WeatherChart.plot(weather);
     }
 
-
-
-    }
+}
